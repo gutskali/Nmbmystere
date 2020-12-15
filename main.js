2582552734113f31bf2981f5fab7fa3072e89044
gutskali <73877303+gutskali@users.noreply.github.com>
2020-12-15 11:02:08 +0100

    nmb mystere

---
+++ main.js
@@ -0,0 +1,10 @@
+// // Créer un jeu simple de devinette de nombre. Le jeu choisit aléatoirement un nombre 
+// // entre 1 et 100, puis il met le joueur au défi de le deviner en 10 tentatives maxi.À chaque tour, le 
+// // joueur doit être informé s'il a deviné ou non le bon nombre — si ce n'est pas le cas, le jeu lui 
+// // indique si son estimation est trop basse ou trop élevée.Le jeu se termine quand le joueur a deviné le nombre mystère, ou 
+// // s'il a épuisé ses 10 chances. À la fin du jeu, le joueur a la possibilité de débuter une nouvelle 
+// // partie.
+// //Bonus : Le jeu doit également rappeler au joueur les 
+// // nombres déjà proposés
+
+let n;
